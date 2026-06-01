a84ce9722f2d8884d95265f8a7daaa00a4aa40d1
Joel Kamiru <kamirujoel2000@gmail.com>
2026-06-01 15:02:58 +0300

    completed the cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 
-const Cart = () => {
+const Cart = ({ cartItems }) => {
   return (
     <div>
       <h2>Shopping Cart</h2>
+      
+    
+      <p>Total Items: {cartItems.length}</p>
+
       <ul>
         {/* TODO: Include items here in li tags with text 'ITEM.NAME is in your cart.' */}
+    
+        {cartItems.map((item, index) => (
+          <li key={index}>
+            {item.name} is in your cart.
+          </li>
+        ))}
       </ul>
+
+      {cartItems.length === 0 && <p>Your cart is empty.</p>}
     </div>
   )
 }
