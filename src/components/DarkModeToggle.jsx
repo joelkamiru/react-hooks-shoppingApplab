906c70ba0eb4a530243617433e9666b9832b37f4
Joel Kamiru <kamirujoel2000@gmail.com>
2026-06-01 14:37:43 +0300

    completed the Toggle theme component

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const DarkModeToggle = () => {
+const DarkModeToggle = ({ isDarkMode, setIsDarkMode }) => {
   // TODO: Implement dark mode toggle logic
+  const handleToggle = () => {
+    setIsDarkMode(!isDarkMode)
+  }
 
   return (
-    <button>Toggle Dark Mode {/* TODO: Update this text from Dark to Light dynamically */}</button>
+    <button onClick={handleToggle}>
+      {/* TODO: Update this text from Dark to Light dynamically */}
+      {isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+    </button>
   )
 }
 
